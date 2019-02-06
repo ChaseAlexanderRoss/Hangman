f92059c1d1ecbd96d40e2d1166788a9bea281541
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-02-06 11:22:11 -0500

    I can store data now

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-//----Geussing Word Array---//
+//----Guessing Word Array---//
 var guessWord = []
 
 //---------Input Box----------//
-var input = document.querySelector('.input')
-input.addEventListener('click', function() {
+var button = document.querySelector('.button')
+button.addEventListener('click', function() {
 //I need the box to take a string of letters and push it into an array
-var guessWord = input.split(" ")
+var preGuess = document.querySelector('.input').value;
+console.log(preGuess)
 })
 
-console.log(guessWord)
