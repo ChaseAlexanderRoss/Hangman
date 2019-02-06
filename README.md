e93ad465e913dad9402fe0be1e6e59ea75d367ab
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-02-06 15:52:18 -0500

    I finally feel like a

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Chase's Hangman Game:
 
 Cited Sources:
 Stole the whole //--Title for thing being worked on--// from the hangman example linked in the repo
-
+I owe W3 schools my life
+Ryan helps fill in the gaps and explains things like a human would 
 Problems:
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-
-//----Guessing Word Array---//
-var guessWord = []
-
 //---------Input Box----------//
 var button = document.querySelector('.button')
 button.addEventListener('click', function() {
-//I need the box to take a string of letters and push it into an array
-var preGuess = document.querySelector('.input').value;
-console.log(preGuess)
-var guessWord = preGuess.split("")
-console.log(guessWord)
+//I need the box to take a string of letters and push it into an array [Done]
+    var preGuess = document.querySelector('.input').value;
+// console.log(preGuess)
+    var guessWord = preGuess.split("")
+// console.log(guessWord)
+//---Creating the hangman stuff--//
+    for (i=0; i<guessWord.length; i++){
+        var div = document.createElement('div')
+        div.className = 'underline'
+        div.innerText = guessWord[i]
+        document.querySelector('.letters').appendChild(div)
+    }
 })
 
